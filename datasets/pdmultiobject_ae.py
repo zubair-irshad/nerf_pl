d89cc52422d750b41804b25be7ea04e503d31f4d
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2022-12-21 13:40:23 -0500

    triplane different architecture single scene

--- datasets/pdmultiobject_ae.py
+++ datasets/pdmultiobject_ae.py
@@ -54,13 +54,13 @@ def read_poses(pose_dir_train, pose_dir_val, img_files, output_boxes = False):
                 all_boxes.append(bbox*pose_scale_factor)
                 
                 #New scene 200 uncomment here
-                # all_rotations.append(data["obj_rotations"][k])
-                # translation = (np.array(data['obj_translations'][k])- obj_location)*pose_scale_factor 
-                # all_translations.append(translation)
+                all_rotations.append(data["obj_rotations"][k])
+                translation = (np.array(data['obj_translations'][k])- obj_location)*pose_scale_factor 
+                all_translations.append(translation)
 
         # Old scenes uncomment here
-        all_translations = (np.array(data['obj_translations'])- obj_location)*pose_scale_factor
-        all_rotations = data["obj_rotations"]
+        # all_translations = (np.array(data['obj_translations'])- obj_location)*pose_scale_factor
+        # all_rotations = data["obj_rotations"]
         
         RTs = {'R': all_rotations, 'T': all_translations, 's': all_boxes}
         return all_c2w_train, all_c2w_test, focal, img_wh, RTs
