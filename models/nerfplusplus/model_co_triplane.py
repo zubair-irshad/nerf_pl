b4ebd3add0620b79a8fd5833dec3d97e150c449e
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-01-26 15:12:12 -0500

    triplanar evaluation code

--- models/nerfplusplus/model_co_triplane.py
+++ models/nerfplusplus/model_co_triplane.py
@@ -395,7 +395,7 @@ class LitNeRFPP_CO_TP(LitModel):
                             'img_wh': tuple(self.hparams.img_wh),
                             'white_back': self.hparams.white_back,
                             'model_type': 'nerfpp',
-                            'eval_inferece': True}
+                            'eval_inference': True}
             self.test_dataset = dataset(split='val',**kwargs_test)
             self.near = self.test_dataset.near
             self.far = self.test_dataset.far
