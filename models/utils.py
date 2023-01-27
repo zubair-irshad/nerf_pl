979219ea017e180d38b3f3d6d417bb618e6c74fc
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-01-26 22:23:53 -0500

    triplanar evaluation code/vis depth

--- models/utils.py
+++ models/utils.py
@@ -41,7 +41,7 @@ def store_depth_img(dirpath, depths):
 
 def store_depth_raw(dirpath, depths):
     depth_maps = []
-    for (i, depth) in enumerate(depths):
+    for (i, depth) in enumerate(depths.detach().cpu().numpy()):
         depthname = f"depth_raw{str(i).zfill(3)}"
         depthpath = os.path.join(dirpath, depthname)
         savez_compressed(depthpath, depth)
